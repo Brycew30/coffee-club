6f5ac4a94d6863667c9826ab7bf0f0a8d114b740
Brycew30 <brycew30@gmail.com>
2020-08-05 10:28:13 -0500

    add helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,14 @@ class ApplicationController < Sinatra::Base
     erb :"/index.html"
   end
 
+  helpers do
+    def signed_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      @user ||= User.find(session[:user_id]) #memoization
+    end
+  end
+
 end
