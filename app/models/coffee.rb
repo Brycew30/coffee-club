0bbfd7d14d94d7c4321485e44e934a22f9a62bf3
Brycew30 <brycew30@gmail.com>
2020-08-10 14:24:22 -0500

    add formatted_created_at method to coffee show page

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -1,6 +1,10 @@
 class Coffee < ActiveRecord::Base
 
   belongs_to :user
-  validates :name, :origin, :variety, :notes, :intensifier, presence: true
+  validates :name, :origin, :variety, :notes, :intensifier, :rating, presence: true
+
+  def formatted_created_at
+    self.created_at.strftime("%a %B %e %Y %l %M %p")
+  end
 
 end
